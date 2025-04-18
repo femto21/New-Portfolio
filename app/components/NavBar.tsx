f0ed51bc7c5a4dd018f39c5c7ec1a96956ee9b3d
Arpit-Gahlot <ar700601@dal.ca>
2025-04-18 00:34:03 -0300

    added global css

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -7,14 +7,17 @@ const NavBar = () => {
         <div className="w-full flex justify-between items-center max-w-9/12 m-auto">
           <div className="flex justify-between">
             <div className="pr-12  border-r-[1.25px] border-[#cabeee]">
-              <Link href="/" className="text-xl  font-medium ">
+              <Link
+                href="/"
+                className="text-xl  font-medium text-color1 text-shadow-nav"
+              >
                 Arpit Gahlot
               </Link>
             </div>
             <div className="ml-12 flex gap-8 justify-between items-center">
               <Link
                 href="/about"
-                className="font-bold text-[#cabeee] text-shadow-nav hover:text-[#aaffe0]"
+                className="font-bold text-color2 text-shadow-nav"
               >
                 About
               </Link>
@@ -23,19 +26,19 @@ const NavBar = () => {
           <div className="flex justify-between gap-6 ">
             <Link
               href="#experience"
-              className="font-medium text-[#b27aee] text-shadow-nav-subtle hover:text-[#aaffe0]"
+              className="font-medium text-color1 text-shadow-nav-subtle "
             >
               Experience
             </Link>
             <Link
               href="#projects"
-              className="font-medium text-[#b27aee] text-shadow-nav-subtle hover:text-[#aaffe0]"
+              className="font-medium text-color1 text-shadow-nav-subtle"
             >
               Projects
             </Link>
             <Link
               href="#contact"
-              className="font-medium text-[#b27aee] text-shadow-nav-subtle hover:text-[#aaffe0]"
+              className="font-medium text-color1 text-shadow-nav-subtle"
             >
               contact
             </Link>

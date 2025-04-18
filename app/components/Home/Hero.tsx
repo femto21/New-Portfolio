527dd95ee9f243ae34e953ff7d501abe0fd90640
Arpit-Gahlot <ar700601@dal.ca>
2025-04-18 01:58:21 -0300

    added cool background

--- app/components/Home/Hero.tsx
+++ app/components/Home/Hero.tsx
@@ -9,8 +9,8 @@ const Hero = () => {
     "Game Developer",
   ];
   return (
-    <div className="w-full flex flex-row gap-8 border-2 border-green-200">
-      <div className="w-full mt-20 m-20 h-96 p-6 border-2 border-pink-200 rounded-4xl overflow-hidden">
+    <div className="w-full flex flex-row gap-8  border-green-200">
+      <div className="w-full mt-20 m-20 h-96 p-6  border-pink-200 rounded-4xl overflow-hidden">
         <Vortex
           backgroundColor="black"
           rangeY={800}
@@ -19,7 +19,7 @@ const Hero = () => {
           baseSpeed={-0.5}
           className="w-full h-full"
         >
-          <div className="p-8 w-full h-full border-2 border-red-300 flex flex-col z-10 ">
+          <div className="p-8 w-full h-full  border-red-300 flex flex-col z-10 ">
             <div className="flex flex-col">
               <div className="pb-8 border-b-[1.25px] border-[#a594f9]">
                 <p className="mb-4 text-5xl  text-color2 text-shadow">

1cac3e3a0600a02dd95fc004edcdba922261aa66
Arpit-Gahlot <ar700601@dal.ca>
2025-04-26 01:19:36 -0300

    fixed projects alignment

--- app/components/Home/ProjectCard.tsx
+++ app/components/Home/ProjectCard.tsx
@@ -17,56 +17,30 @@ const ProjectCard = (props: Project) => {
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className="h-96 w-96 relative p-8 rounded-2xl bg-black dark:hover:shadow-2xl dark:hover:shadow-violet-500/[0.1]"
+      className="h-96 w-full p-3 rounded-2xl flex flex-row hover:shadow-2xl transition-all duration-300 ease-out hover:shadow-violet-500/[0.1]"
     >
-      <img
-        src={props.image}
-        className={cn(
-          "object-cover absolute inset-0 rounded-2xl transition-all duration-300 ease-out",
-          hovered === true ? "opacity-30 " : "opacity-80"
-        )}
-        alt={props.title}
-      />
+      <img src={props.image} className="rounded-xl h-90 " alt={props.title} />
+      <div className="px-4 ">
+        <div className="flex flex-row items-center justify-center">
+          <p className="text-4xl relative z-20 text-color1 text-shadow">
+            {props.title}
+          </p>
+        </div>
 
-      <div className="flex flex-row items-center justify-center">
-        <p
-          className={cn(
-            "text-4xl relative z-20 mt-6 text-color1 text-shadow",
-            !hovered && "opacity-0"
-          )}
-        >
-          {props.title}
-        </p>
-      </div>
+        <div className="text-color2 text-shadow mt-4 relative z-20">
+          {props.description}
+        </div>
+        <div className="text-color2 text-shadow relative z-20 flex flex-row flex-wrap">
+          {props.tech.map((currentTech) => {
+            return <p key={currentTech}>#{currentTech}&nbsp;</p>;
+          })}
+        </div>
 
-      <div
-        className={cn(
-          "text-color2 text-shadow mt-4 relative z-20",
-          !hovered && "opacity-0"
-        )}
-      >
-        {props.description}
-      </div>
-      <div
-        className={cn(
-          "text-color2 text-shadow relative z-20 flex flex-row flex-wrap",
-          !hovered && "opacity-0"
-        )}
-      >
-        {props.tech.map((currentTech) => {
-          return <p key={currentTech}>#{currentTech}&nbsp;</p>;
-        })}
-      </div>
-
-      <div
-        className={cn(
-          "text-l text-color1 text-shadow relative flex flex-row-reverse z-20",
-          !hovered && "opacity-0"
-        )}
-      >
-        <a href={props.url} target="_blank">
-          Github
-        </a>
+        <div className="text-l text-color1 text-shadow relative flex flex-row-reverse z-20">
+          <a href={props.url} target="_blank">
+            Github
+          </a>
+        </div>
       </div>
     </div>
   );
--- app/components/Home/Projects.tsx
+++ app/components/Home/Projects.tsx
@@ -28,7 +28,7 @@ const Projects = async () => {
     <section>
       <div className="m-auto w-full">
         <p className="text-4xl text-color1 text-shadow">My Projects</p>
-        <div className="mt-10 flex flex-row flex-wrap justify-between">
+        <div className="mt-10 flex flex-col gap-8 justify-between">
           {projects.map((project: Project) => {
             return <ProjectCard {...project} key={project.id} />;
           })}

5e251f8f99ce44c42ae16bb44604aa859eb43c7d
Arpit-Gahlot <ar700601@dal.ca>
2025-04-28 00:09:41 -0300

    brain not braining

--- app/components/Home/ProjectCard.tsx
+++ app/components/Home/ProjectCard.tsx
@@ -19,7 +19,7 @@ const ProjectCard = (props: Project) => {
       <div
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
-        className="h-96 w-full relative p-6 rounded-2xl  flex flex-row hover:shadow-2xl transition-all duration-300 ease-out hover:shadow-violet-500/[0.1]"
+        className="h-96 w-full relative p-6 rounded-2xl flex flex-row hover:shadow-2xl transition-all duration-300 ease-out hover:shadow-violet-500/[0.1]"
       >
         <div
           className={`absolute w-full h-full top-0 left-0 bg-white transition-opacity rounded-2xl ease-out duration-300 ${
@@ -31,34 +31,34 @@ const ProjectCard = (props: Project) => {
           className="rounded-xl z-20 hover:scale-110 duration-300 ease-out"
           alt={props.title}
         />
-        <div className="px-4 z-20">
-          <div className="flex flex-row items-center justify-center">
-            <p className="text-4xl relative z-20 text-color1 text-shadow">
+        <div className="px-4 z-20  flex flex-col justify-between h-full border-2">
+          <div className="flex mb-2 flex-row items-center justify-center">
+            <p className="text-4xl  z-20 text-color1 text-shadow">
               {props.title}
             </p>
           </div>
-          <div className="flex flex-row gap-2">
+          <div className="flex flex-row  gap-2">
             {props.tags.map((tag) => {
               return (
                 <div
                   key={tag.id}
-                  className="bg-gray-600 px-2 py-1 text-color2 text-shadow rounded-lg"
+                  className="bg-gray-600 px-2 py-1 text-color2  text-shadow rounded-lg"
                 >
                   {tag.name}
                 </div>
               );
             })}
           </div>
-          <div className="text-color2 text-shadow relative z-20 flex flex-row flex-wrap">
+          <div className="text-color2 text-shadow z-20 flex flex-row flex-wrap">
             {props.tech.map((currentTech) => {
               return <p key={currentTech}>#{currentTech}&nbsp;</p>;
             })}
           </div>
-          <div className="text-color2 text-shadow mt-4 relative z-20">
+          <div className="text-color2 text-shadow mt-4  z-20">
             {props.description}
           </div>
 
-          <div className="text-l text-color1 text-shadow relative flex flex-row-reverse z-20">
+          <div className="text-l h-full text-color1 text-shadow  z-20">
             <a href={props.url} target="_blank">
               Github
             </a>

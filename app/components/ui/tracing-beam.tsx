44a1635c47d7e7afa3109e8496c5d291fa257811
Arpit-Gahlot <ar700601@dal.ca>
2025-04-28 23:29:52 -0300

    card looking better

--- app/components/ui/tracing-beam.tsx
+++ app/components/ui/tracing-beam.tsx
@@ -54,24 +54,21 @@ export const TracingBeam = ({
           aria-hidden="true"
         >
           <motion.path
-            d={`M 1 0V -36 l 18 24 V ${svgHeight * 0.8} l -18 24V ${svgHeight}`}
+            d={`M 18 0 V ${svgHeight}`}
             fill="none"
             stroke="#9091A0"
             strokeOpacity="0.16"
-            transition={{
-              duration: 10,
-            }}
-          ></motion.path>
+            transition={{ duration: 10 }}
+          />
+
           <motion.path
-            d={`M 1 0V -36 l 18 24 V ${svgHeight * 0.8} l -18 24V ${svgHeight}`}
+            d={`M 18 0 V ${svgHeight}`}
             fill="none"
             stroke="url(#gradient)"
             strokeWidth="2"
             className="motion-reduce:hidden"
-            transition={{
-              duration: 10,
-            }}
-          ></motion.path>
+            transition={{ duration: 10 }}
+          />
           <defs>
             <motion.linearGradient
               id="gradient"
